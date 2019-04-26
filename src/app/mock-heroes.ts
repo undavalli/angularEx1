506615d3207097534cf93e552501383df09c370b
tkmaivf <prudhviraj.undavalli@kohls.com>
2019-04-26 13:52:41 -0500

    my first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id:11, name:'prudhvi'},
+    {id:12, name:'raj'},
+    {id:13, name:'undavalli'},
+    {id:14, name:'prudhviraj'},
+    {id:15, name:'undavalli1'}
+    ];
