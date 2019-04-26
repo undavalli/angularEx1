aa0aa72f29e0b597b90404623a08de7ac461d77b
tkmaivf <prudhviraj.undavalli@kohls.com>
2019-04-26 14:37:05 -0500

    generate messages service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,15 +15,20 @@ export class HeroesComponent implements OnInit {
     name: 'windstorm'
   };
 
-  heroes=HEROES;
+  heroes:Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
   }
 
 }
