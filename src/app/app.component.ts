5a60c1f7862efc9b05b9cbabf6645746eeb86b79
tkmaivf <prudhviraj.undavalli@kohls.com>
2019-04-27 09:47:52 -0500

    adding routing information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-import {HeroesComponent} from './heroes/heroes.component';
+import { Component, OnInit } from '@angular/core';
+import { HeroesComponent } from './heroes/heroes.component';
+import { ActivatedRoute  } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour-of-heroes';
+
+  constructor(private router: ActivatedRoute){
+
+  }
+
+  ngOnInit(){
+    this.router.queryParams.subscribe(params => {
+      console.log(params);
+      console.log("window.location="+window.location);
+  })
+  }
 }
